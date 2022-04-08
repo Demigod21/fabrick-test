8c543c95365991bc9b246243b273ef2b6491acc2
Demigod21 <davide.falcone276@gmail.com>
2022-04-08 08:57:04 +0200

    spring inizializr and gitignore

---
+++ src/test/java/com/davide/falcone/fabricktest/FabrickTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.davide.falcone.fabricktest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FabrickTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
